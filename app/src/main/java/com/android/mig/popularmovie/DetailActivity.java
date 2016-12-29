9225333bb0745635a8003093a3964424865ab4e3
Miguel Saenz <msaenz424@gmail.com>
2016-12-29 12:22:33 -0800

    defined colors for DetailActivity

--- app/src/main/java/com/android/mig/popularmovie/DetailActivity.java
+++ app/src/main/java/com/android/mig/popularmovie/DetailActivity.java
@@ -4,13 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
 
-    private TextView tvTitle, tvYearRelease, tvRating, tvSynopsis;
+    private TextView tvTitle, tvYearRelease, tvSynopsis;
+    private RatingBar rbRating;
     private ImageView ivPoster;
     private Movie mMovie;
 
@@ -21,8 +23,8 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         tvTitle = (TextView) findViewById(R.id.tv_title);
         ivPoster = (ImageView) findViewById(R.id.iv_poster);
-        tvYearRelease = (TextView) findViewById(R.id.tv_year_release);
-        tvRating = (TextView) findViewById(R.id.tv_rating);
+        tvYearRelease = (TextView) findViewById(R.id.tv_release_date);
+        rbRating = (RatingBar) findViewById(R.id.rb_movies);
         tvSynopsis = (TextView) findViewById(R.id.tv_synopsis);
 
         Intent intent = getIntent();
@@ -32,7 +34,7 @@ public class DetailActivity extends AppCompatActivity {
                 tvTitle.setText(mMovie.getTitle());
                 Picasso.with(this).load(NetworkUtils.BASE_URL + NetworkUtils.IMAGE_SIZE + mMovie.getPosterPath()).into(ivPoster);
                 tvYearRelease.setText(mMovie.getReleaseDate());
-                tvRating.setText(mMovie.getRating().toString());
+                rbRating.setRating(Float.parseFloat(mMovie.getRating().toString()));
                 tvSynopsis.setText(mMovie.getPlotSynopsis());
             }
         }
